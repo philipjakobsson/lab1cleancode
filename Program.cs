7bfc672a97e20c060dba823edbd1c39571352307
philipjakobsson <71271470+philipjakobsson@users.noreply.github.com>
2021-12-20 11:26:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1cleancode
+{
+    class FizzBuzz
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.Write("Fizz");
+                if (i % 5 == 0)
+                    Console.Write("Buzz");
+                if (!(i % 3 == 0 || i % 5 == 0))
+                    Console.Write(i);
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+    }
+}
