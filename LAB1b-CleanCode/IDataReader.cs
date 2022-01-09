e3fe51add450f45cac0156fd022923c845f2405b
philipjakobsson <71271470+philipjakobsson@users.noreply.github.com>
2022-01-09 15:50:55 +0100

    Add files via upload

---
+++ LAB1b-CleanCode/IDataReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1b_Terminal
+{
+    public interface IDataReader
+    {
+        string[] GetStrings();
+        string[] ReadFile();
+    }
+}
---
+++ LAB1b-CleanCode/Labb1b-Test/FileHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using Labb1b_Terminal;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1b_Test
+{
+    [TestFixture]
+    public class FileHandlerTests
+    {
+        private readonly string[] _list = new string[] { "Philip", "Lars", "Sven", "Alfred", "Göran" };
+
+        [Test]
+        public void ReadFile_ReadLinesFromLocalTxtFile_ReturnsTrueIfNotNull()
+        {
+            Mock<IDataReader> mock = new Mock<IDataReader>();
+            mock.Setup(x => x.ReadFile()).Returns(_list);
+
+            IDataReader fileHandler = mock.Object;
+            var textArr = fileHandler.ReadFile();
+
+            Assert.NotNull(textArr);
+        }
+    }
+}
