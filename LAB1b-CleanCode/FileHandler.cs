637fb7cb0151e8548e9c4a0bbc68adbaa1ed48d6
philipjakobsson <71271470+philipjakobsson@users.noreply.github.com>
2022-01-11 12:26:13 +0100

    Add files via upload

---
+++ LAB1b-CleanCode/FileHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Labb1b_Terminal
+{
+    public class FileHandler : IDataReader
+    {
+        public string[] GetStrings()
+        { 
+            //Change path so that it matches your repo
+            string[] textArr = File.ReadAllLines(@"C:\Users\phili\Source\Repos\LAB1b-CleanCode\indata.txt");
+
+            return textArr;
+        }
+
+        public string[] ReadFile()
+        {
+            try
+            {
+                string[] textArr = GetStrings();
+
+                if (textArr.Length == 0)
+                {
+                    throw new Exception("File is empty");
+                }
+                else
+                {
+                    return textArr;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+    }
+}
+
